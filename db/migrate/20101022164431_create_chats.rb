8cef6aa3bcb9e1b253ea156259173e374590f22a
kuznetsov_i <3damned@gmail.com>
2017-05-27 01:12:11 +0300

    Prepare new table structure for holding project-scope and private messages

--- db/migrate/20101022164431_create_chats.rb
+++ db/migrate/20101022164431_create_chats.rb
@@ -2,7 +2,9 @@ class CreateChats < ActiveRecord::Migration
   def self.up
     create_table :chats do |t|
       t.column :message, :string
-      t.column :user, :integer
+      t.column :user, :integer				# from user 
+      t.column :user2, :integer				# to user - NULL or 0 means the message to all users in scope of project_id
+      t.column :project_id, :integer		# project_id - NULL describes messages in default public chat 
       t.column :created_at, :timestamp
     end
   end
